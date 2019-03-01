f52164b41ac86c21d1d860714a6b875bca36f399
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-02-28 23:08:40 -0300

    Página de projeto

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.querySelector("#projetos").scrollIntoView({ behavior: 'smooth' });
     }
 });
+
+function mostrarMenu(){
+    document.querySelector("#barraSuperior").classList.add("ativo");
+}
+
+function esconderMenu(){
+    document.querySelector("#barraSuperior").classList.remove("ativo");
+}
+
+function toggleMenu(){
+    if(document.querySelector("#barraSuperior").classList.contains("ativo")){
+        esconderMenu();
+    } else{
+        mostrarMenu();
+    }
+}
