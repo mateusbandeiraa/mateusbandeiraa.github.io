70115b933cec4165acc8b91b52d95ebc4658d718
mateusbandeiraa <mateusbandeiraa@gmail.com>
2019-03-06 13:48:01 -0300

    Ambiente Dev/Prod
    Script.js checa se está em ambiente de desenvolvimento ou produção.
    Se estiver em desenvolvimento, ativa o módulo live.js.
    Se estiver em produção, ativa o Google Analytics.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-function mostrarMenu(){
+const ambiente = /mateusbandeira\.dev/.test(document.location.hostmane) ? "prod" : "dev";
+if (ambiente == "dev") {
+    import("/js/live.js").then((livejs) => { livejs.default() }); // Atualiza a página automaticamente se houver mudanças no HTML, CSS ou JS.
+} else { // prod
+    // Google Analytics
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-135366133-1');
+}
+
+function mostrarMenu() {
     document.querySelector("#barraSuperior").classList.add("ativo");
 }
 
-function esconderMenu(){
+function esconderMenu() {
     document.querySelector("#barraSuperior").classList.remove("ativo");
 }
 
-function toggleMenu(){
-    if(document.querySelector("#barraSuperior").classList.contains("ativo")){
+function toggleMenu() {
+    if (document.querySelector("#barraSuperior").classList.contains("ativo")) {
         esconderMenu();
-    } else{
+    } else {
         mostrarMenu();
     }
 }
